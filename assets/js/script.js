1b3019b9c8c758140630cb9e44e61e83b9e75275
leonardo <leo.cirillo92@gmail.com>
2023-11-21 22:27:50 +0100

    add img and style

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// function go() {
+//   let mydiv = document.getElementById("btnBox");
+
+//   let button = document.createElement('BUTTON');
+
+//   let text = document.createTextNode('button');
+
+//   button.appendChild(text);
+//   mydiv.appendChild(button);;
+// }
